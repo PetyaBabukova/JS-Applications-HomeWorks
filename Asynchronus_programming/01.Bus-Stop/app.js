18f17c71b6ab56cf7fd4c586143e55e388137dae
Petya Babukova <p_babukova@abv.bg>
2024-02-15 19:31:36 +0200

    Bus Stop - 75%

--- Asynchronus_programming/01.Bus-Stop/app.js
+++ Asynchronus_programming/01.Bus-Stop/app.js
@@ -1,3 +1,38 @@
 function getInfo() {
-    console.log("TODO...");
+
+    let busesListEl = document.getElementById('buses');
+                busesListEl.innerHTML = '';
+
+    let baseUrl = 'http://localhost:3030/jsonstore/bus/businfo';
+
+    let stopIDElement = document.getElementById('stopId');
+    let stopId = stopIDElement.value;
+
+    fetch(`${baseUrl}/${stopId}`)
+        .then(res => res.json()
+            .then(data => {
+                // console.log(data)
+                let buses = data.buses;
+
+                let stopName = data.name;
+
+                let busStopEl = document.getElementById('stopName');
+                busStopEl.textContent = stopName;
+
+                let busesListEl = document.getElementById('buses');
+                busesListEl.innerHTML = '';
+
+                Object.entries(buses)
+                    .forEach(([busId, time]) => {
+
+                        let contentLi = `Bus ${busId} arrives in ${time} minutes`;
+
+                        let liEl = document.createElement('li');
+                        liEl.textContent = contentLi;
+                        // console.log(contentLi);
+                        busesListEl.appendChild(liEl);
+
+                    })
+            }))
+        .catch(error => busStopEl.textContent = 'Error');
 }
