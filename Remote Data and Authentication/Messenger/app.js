832dee4ea00aaafc78ea669b45672ed8634fb774
Petya Babukova <p_babukova@abv.bg>
2024-02-21 13:19:08 +0200

    Remote data and authentication exercise - initial tasks

---
+++ Remote Data and Authentication/Messenger/app.js
@@ -0,0 +1,55 @@
+function attachEvents() {
+    let sendBtn = document.getElementById('submit');
+    sendBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        let name = document.querySelector('input[name="author"]').value;
+        let message = document.querySelector('input[name="content"]').value;
+
+        let data = {
+            author: name,
+            content: message,
+        };
+
+        fetch('http://localhost:3030/jsonstore/messenger', {
+            method: 'POST', 
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(res => {
+            if (!res.ok) {
+                throw new Error('Failed to send message');
+            }
+            return res.json();
+        })
+        .then(() => {
+            document.querySelector('input[name="author"]').value = '';
+            document.querySelector('input[name="content"]').value = '';
+        })
+        .catch(error => console.error('Error:', error)); 
+    });
+
+    let refreshBtn = document.getElementById('refresh');
+    refreshBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        let textarea = document.getElementById('messages');
+        textarea.value = ''; 
+        fetch('http://localhost:3030/jsonstore/messenger')
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error('Failed to fetch messages');
+                }
+                return res.json();
+            })
+            .then(data => {
+                Object.values(data).forEach(x => {
+                    textarea.value += `${x.author}: ${x.content}\n`;
+                });
+            })
+            .catch(error => console.error('Error:', error)); 
+    });
+}
+
+attachEvents();
